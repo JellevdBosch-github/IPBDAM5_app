420e55e0a0c2d0a91c3c4c16f3172103c806935b
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-06-09 02:59:53 +0200

    Updated app.py; Test route and fixed config updating

--- api/app.py
+++ api/app.py
@@ -2,22 +2,30 @@ import sys
 
 from flask import Flask, jsonify
 from flask_cors import CORS
-from api import config
+from api.config import config
 
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+@app.route('/')
+def index():
+	return jsonify('hello flask')
+
+
 if __name__ == '__main__':
 	env = sys.argv[1] if len(sys.argv) > 2 else 'dev'
 
 	if env == 'dev':
-		app.config = config.DevConfig
+		print('dev')
+		app.config.from_object(config.DevConfig)
 	elif env == 'test':
-		app.config = config.TestConfig
+		print('test')
+		app.config.from_object(config.TestConfig)
 	elif env == 'prod':
-		app.config = config.ProdConfig
+		print('prod')
+		app.config.from_object(config.ProdConfig)
 	else:
 		raise ValueError('Invalid environment name')
 
