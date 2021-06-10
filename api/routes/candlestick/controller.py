6eb0314f682b27223603afa407562c6db79f9637
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-06-10 09:35:44 +0200

    [API] Added Flask_RESTful package to route controllers

--- api/routes/candlestick/controller.py
+++ api/routes/candlestick/controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from flask_restful import Resource, Api
 from utils import datetime
 
 
--- api/routes/currency/controller.py
+++ api/routes/currency/controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_restful import Resource, Api
+from utils import datetime
 
 
 module_currency = Blueprint('currency', __name__, url_prefix='/api/currency')
