13acd3682fc1d90213b6debae080e2cd3ba33785
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-06-10 02:37:21 +0200

    [API] Added candlestick endpoint routes

--- api/routes/candlestick/controller.py
+++ api/routes/candlestick/controller.py
@@ -1,4 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from utils import datetime
 
 
 module_candlestick = Blueprint('candlestick', __name__, url_prefix='/api/candlestick')
+
+
+@module_candlestick.route('/browse', methods=['GET'])
+def browse_candlesticks():
+	pass
+	# response_object = {
+	# 	'status': 'Failed',
+	# 	'error': 'Invalid request header',
+	# 	'description': 'This endpoint requires a GET request'
+	# }
+
+
+@module_candlestick.route('/browse/<int:timestamp>', methods=['POST'])
+def browse_candlesticks(timestamp):
+	pass
+
+
+@module_candlestick.route('/read/', defaults={'candlestick_id': datetime.get_current_epoch_ms()})
+@module_candlestick.route('/read/<candlestick_id>', methods=['POST'])
+def read_candlestick(candlestick_id):
+	pass
