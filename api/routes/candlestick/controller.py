4003939bea3993e5e3b0d0dcf9cfeaca6f6c718a
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-06-24 21:08:06 +0200

    [API] Added more candlesticks

--- api/routes/candlestick/controller.py
+++ api/routes/candlestick/controller.py
@@ -6,53 +6,101 @@ from . import api_candlestick
 
 
 OHLC = [
+{
+		'candlestick_id': '7',
+		'timestamp': '1624539599999',
+		'open': 0.20326000,
+		'high': 0.20539000,
+		'low': 0.19966000,
+		'close': 0.20281000
+	},
+	{
+		'candlestick_id': '8',
+		'timestamp': '1624535999999',
+		'open': 0.19586000,
+		'high': 0.20363000,
+		'low': 0.19586000,
+		'close': 0.20318000
+	},
+	{
+		'candlestick_id': '9',
+		'timestamp': '1624532399999',
+		'open': 0.19941000,
+		'high': 0.20000000,
+		'low': 0.19561000,
+		'close': 0.19593000
+	},
+	{
+		'candlestick_id': '10',
+		'timestamp': '1624528799999',
+		'open': 0.20027000,
+		'high': 0.20125000,
+		'low': 0.19755000,
+		'close': 0.19897000
+	},
+	{
+		'candlestick_id': '11',
+		'timestamp': '1624525199999',
+		'open': 0.19520000,
+		'high': 0.19520000,
+		'low': 0.19520000,
+		'close': 0.19983000
+	},
+	{
+		'candlestick_id': '12',
+		'timestamp': '1624521599999',
+		'open': 0.19629000,
+		'high': 0.19995000,
+		'low': 0.19455000,
+		'close': 0.19492000
+	},
 	{
 		'candlestick_id': '1',
-		'timestamp': '1623300785000',
-		'open': 123,
-		'high': 145,
-		'low': 118,
-		'close': 140
+		'timestamp': '1624543199999',
+		'open': 0.20270000,
+		'high': 0.20300000,
+		'low': 0.19791000,
+		'close': 0.20054000
 	},
 	{
 		'candlestick_id': '2',
-		'timestamp': '1623304385000',
-		'open': 140,
-		'high': 155,
-		'low': 138,
-		'close': 150
+		'timestamp': '1624546799999',
+		'open': 0.20065000,
+		'high': 0.20133000,
+		'low': 0.19800000,
+		'close': 0.20123000
 	},
 	{
 		'candlestick_id': '3',
-		'timestamp': '1623307985000',
-		'open': 150,
-		'high': 152,
-		'low': 141,
-		'close': 144
+		'timestamp': '1624550399999',
+		'open': 0.20130000,
+		'high': 0.20635000,
+		'low': 0.19980000,
+		'close': 0.20096000
 	},
-{
+	{
 		'candlestick_id': '4',
-		'timestamp': '1623311585000',
-		'open': 144,
-		'high': 145,
-		'low': 118,
-		'close': 140
+		'timestamp': '1624553999999',
+		'open': 0.20071000,
+		'high': 0.20487000,
+		'low': 0.19881000,
+		'close': 0.20483000
 	},
 	{
 		'candlestick_id': '5',
-		'timestamp': '1623315185000',
-		'open': 140,
-		'high': 155,
-		'low': 138,
-		'close': 150
+		'timestamp': '1624557599999',
+		'open': 0.20484000,
+		'high': 0.20749000,
+		'low': 0.20315000,
+		'close': 0.20588000
 	},
 	{
 		'candlestick_id': '6',
-		'timestamp': '1623523973068',
-		'open': 150,
-		'high': 152,
-		'low': 141,
-		'close': 144
+		'timestamp': '1624561199999',
+		'open': 0.20610000,
+		'high': 0.20968000,
+		'low': 0.20490000,
+		'close': 0.20914000
 	},
 ]
 
