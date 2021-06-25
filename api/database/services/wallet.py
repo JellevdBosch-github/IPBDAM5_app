cc5c8d32e72bde0df3d27873303b7a0ccccf928f
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-06-25 19:23:34 +0200

    [API] Wallet endpoints pointing now to database services, removed timestamp for now

---
+++ api/database/services/wallet.py
@@ -0,0 +1,21 @@
+from . import conn as connection
+from utils.datetime import get_current_epoch_ms
+
+
+def get(wallet_id=1):
+	with connection.connect() as conn:
+		results = conn.execute(
+			f'SELECT * FROM wallet WHERE wallet_id = {wallet_id};'
+		)
+	result = []
+	for r in results:
+		result.append(
+			{
+				'wallet_id': r[0],
+				'eur_value': r[1],
+				'usd_value': r[2],
+				'doge_value': r[3],
+			}
+		)
+	print(result)
+	return result
--- api/routes/wallet/controller.py
+++ api/routes/wallet/controller.py
@@ -3,29 +3,20 @@ from flask_restful import Resource, Api, abort
 from utils import datetime
 from . import module_wallet
 from . import api_wallet
+from database.services import wallet as service
 
 
-WALLET = [
-	{
-		'wallet_id': '1',
-		'eur_value': 10000,
-		'usd_value': 11871.70,
-		'doge_value': 62310.3662,
-	},
-]
-
-
-def abort_not_found(wallet_id):
-	if not any(wallet['wallet_id'] == wallet_id for wallet in WALLET):
-		abort(
-			404,
-			message={
-				'status': 'failed',
-				'endpoint': '/api/wallet/<wallet_id>',
-				'request_method': 'get',
-				'error_message': f'No wallet found matching the given id ({wallet_id})!'
-			}
-		)
+# def abort_not_found(wallet_id):
+# 	if not any(wallet['wallet_id'] == wallet_id for wallet in WALLET):
+# 		abort(
+# 			404,
+# 			message={
+# 				'status': 'failed',
+# 				'endpoint': '/api/wallet/<wallet_id>',
+# 				'request_method': 'get',
+# 				'error_message': f'No wallet found matching the given id ({wallet_id})!'
+# 			}
+# 		)
 
 
 class Wallet(Resource):
@@ -35,33 +26,33 @@ class Wallet(Resource):
 
 	@staticmethod
 	def get(wallet_id):
-		abort_not_found(wallet_id)
+		# abort_not_found(wallet_id)
 		return jsonify(
 			status='success',
 			endpoint='/api/wallet/<wallet_id>',
 			request_method='get',
-			wallet=[wallet for wallet in WALLET if wallet['wallet_id'] == wallet_id]
+			wallet=[wallet for wallet in service.get(wallet_id)]
 		)
 
 
 api_wallet.add_resource(Wallet, '/<wallet_id>')
 
 
-class WalletTime(Resource):
-	"""
-	Returns the candlestick with the given id
-	"""
-
-	@staticmethod
-	def get(wallet_id):
-		abort_not_found(wallet_id)
-		return jsonify(
-			status='success',
-			endpoint='/api/wallet/<wallet_id>/<timestamp>',
-			request_method='get',
-			# TODO: return wallet value at certain point in time
-			wallet=[wallet for wallet in WALLET if wallet['wallet_id'] == wallet_id]
-		)
-
-
-api_wallet.add_resource(WalletTime, '/<wallet_id>/<timestamp>')
+# class WalletTime(Resource):
+# 	"""
+# 	Returns the candlestick with the given id
+# 	"""
+#
+# 	@staticmethod
+# 	def get(wallet_id):
+# 		abort_not_found(wallet_id)
+# 		return jsonify(
+# 			status='success',
+# 			endpoint='/api/wallet/<wallet_id>/<timestamp>',
+# 			request_method='get',
+# 			# TODO: return wallet value at certain point in time
+# 			wallet=[wallet for wallet in WALLET if wallet['wallet_id'] == wallet_id]
+# 		)
+#
+#
+# api_wallet.add_resource(WalletTime, '/<wallet_id>/<timestamp>')
